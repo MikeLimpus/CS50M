fa573fab0cabb03154b52dbc68dd8578f9f815c5
Mike Limpus <mikelimpus111@gmail.com>
2018-07-28 18:35:07 -0700

    Worked on project

--- project0/script.js
+++ project0/script.js
@@ -5,10 +5,19 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+let todoList = []; //Array to store todos
+
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  /*
+   *code will go here
+   *may want to use dynamic arrays using push function
+   *to store todos as strings
+   */ 
+  let todo = window.prompt(todo);
+  todoList.push(todo)
+  //alert('New TODO button clicked!')
 }
