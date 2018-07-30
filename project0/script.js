64bc2fa089ac1592c38accb9163be8e66f8e900c
Mike Limpus <mikelimpus111@gmail.com>
2018-07-30 11:25:18 -0700

    Cleaned up code

--- project0/script.js
+++ project0/script.js
@@ -12,20 +12,14 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-function newTodo() {
-  /*
-   *code will go here
-   *may want to use dynamic arrays using push function
-   *to store todos as strings
-   */   
+function newTodo() {   
 	listIndex++; 			//increment list index, first function call should = 1
-  //Input string to variable
-	let toDoString = prompt('Input what you need to do here'); 
+  /*Input string to variable*/
+	let toDoString = window.prompt('Input what you need to do here'); 
 	/*Object to be defined as a TODO, includes string with info*/ 
 	const todo = {}
 		todo.name = "TODO " + listIndex; 
 		todo.info = toDoString;
   todoList.push(todo);
-
   //alert('New TODO button clicked!')
 }
