f42d66e5581ff9aa045bfe5a733d605e7a94ca16
MikeLimpus <mikelimpus111@gmail.com>
2018-08-02 18:15:27 -0700

    Made progress on project 0 and started Lesson 3

---
+++ 2 React, Props, State/2-Set.js
@@ -0,0 +1,20 @@
+//We can also extend the native implementation of Set if we wanted to 
+//do something like log on addition or create new methods
+
+class MySet extends Set {
+    constructor(arr) {
+        super(arr)
+        this.originalArray = arr
+    }
+    add(val) {
+        super.add(val)
+        console.log('added ${val} to the set')
+    }
+
+    toArray() {
+        return Array.from(this)
+    }
+    reset() {
+        return new MySet(this.originalArray)
+    }
+}
--- project0/script.js
+++ project0/script.js
@@ -13,13 +13,22 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {   
-	listIndex++; 			//increment list index, first function call should = 1
+  listIndex++; 			//increment list index, first function call should = 1
+  itemCountSpan.textContent = listIndex;
   /*Input string to variable*/
 	let toDoString = window.prompt('Input what you need to do here'); 
 	/*Object to be defined as a TODO, includes string with info*/ 
 	const todo = {}
 		todo.name = "TODO " + listIndex; 
-		todo.info = toDoString;
+    todo.info = toDoString;
+  list.innerHTML += '<input type="checkbox" name="TODO" value="TODO">' + todo.info + '<br>';
+    //"<h4>" + todo.name + "</h4>"  +
+    //"<p>" + todo.info + "</p>";
   todoList.push(todo);
-  //alert('New TODO button clicked!')
 }
+
+
+/*
+ * Known bugs:
+ * Item count is incremented regardless of whether new item is actually added
+ */
