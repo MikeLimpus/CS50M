e740ab87e058831f48c193fb5a25ac101dad5116
Mike Limpus <mikelimpus111@gmail.com>
2018-08-07 14:02:14 -0700

    cleaned up code and added checked as a variable in html todo checkbox element

--- project0/script.js
+++ project0/script.js
@@ -1,7 +1,6 @@
 let todoList = []; 	//Array to store todos
 let listIndex = 0;	//Used to store number of items in the list
 let uncheckedListIndex = 0;
-let bool = false;
 
 const classNames = {
   TODO_ITEM: 'todo-container',
@@ -20,17 +19,24 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 function newTodo() {   
 	listIndex++; 			//increment list index, first function call should = 1
 	uncheckedListIndex++;
-	let x;		//temp variable for checkbox bool
+	let check = false;
 	itemCountSpan.textContent = listIndex;
 	uncheckedCountSpan.textContent = uncheckedListIndex;
-  /*Input string to variable*/
+  	/*Input string to variable*/
 	let toDoString = window.prompt('Input what you need to do here'); 	
 	/*Object to be defined as a TODO, includes string with name and info*/ 
 	const todo = {} 
 		todo.name = "TODO" + listIndex;
 		todo.info = toDoString;
 
-	list.innerHTML += '<input type="checkbox" class="todo-checkbox" name= "TODO ' + listIndex + '"value="TODO" onClick="checker()">' + todo.info + '<br>';
+	list.innerHTML += `
+		<input type="checkbox" 
+		checked="${check}
+		class="todo-checkbox" 
+		name= "TODO ${listIndex}"
+		value="TODO" onClick="checker()"> 
+		${todo.info} <br>`;
+	//const checkBox = document.createElement('div');
 	
 	todoList.push(todo);
 	
