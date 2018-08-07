df4a4025deecb8c3aeb00a01c10a453bd8f90306
Mike Limpus <mikelimpus111@gmail.com>
2018-08-06 20:03:42 -0700

    Added a junk file for all prototype code

---
+++ project0/junkcode.js
@@ -0,0 +1,18 @@
+function check() {
+	bool = true;
+	console.log('check')
+}
+function uncheck() {
+	bool = false;
+	console.log('uncheck') 
+}
+
+function switcher() {
+	console.log('switched')
+	if (bool = false) {
+		check();
+		}
+	else if (bool = true){
+		uncheck();
+	}
+}
--- project0/script.js
+++ project0/script.js
@@ -1,16 +1,27 @@
 let todoList = []; 	//Array to store todos
 let listIndex = 0;	//Used to store number of items in the list
-class ToDo {
-  constructor(name, info) {
+let uncheckedListIndex = 0;
+let bool = false;
+/*class ToDo {
+  constructor(name, info, check) {
     this.name = name;
-    this.info = info;
+		this.info = info;
+		this.check = Boolean(check);
   }
-  
-  isChecked(elementName) { //TODO: fix this
-    return document.getElementById(elementName).checked;
-  }
-}
-
+	checker() {
+		if(this.check = true) {
+			this.check = Boolean(false);
+			console.log("this check should now equal false")
+		}
+		else if (this.check = false) {
+			this.check = Boolean(true);
+			console.log("this check should now equal true")
+		}
+		else {
+			console.log("This checker() isn't working")
+		}
+	}
+}*/
 const classNames = {
   TODO_ITEM: 'todo-container',
   TODO_CHECKBOX: 'todo-checkbox',
@@ -18,28 +29,37 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-
-
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+
+
 function newTodo() {   
-  listIndex++; 			//increment list index, first function call should = 1
-  itemCountSpan.textContent = listIndex;
+	listIndex++; 			//increment list index, first function call should = 1
+	uncheckedListIndex++;
+	let x;		//temp variable for checkbox bool
+	itemCountSpan.textContent = listIndex;
+	uncheckedCountSpan.textContent = uncheckedListIndex;
   /*Input string to variable*/
-	let toDoString = window.prompt('Input what you need to do here'); 
+	let toDoString = window.prompt('Input what you need to do here'); 	
 	/*Object to be defined as a TODO, includes string with name and info*/ 
-  const todo = new ToDo("TODO" + listIndex, toDoString);
-
-  list.innerHTML += '<input type="checkbox" id="TODO ${listIndex}" value="TODO">' + todo.info + '<br>';
-    //"<h4>" + todo.name + "</h4>"  +
-    //"<p>" + todo.info + "</p>";
-  todoList.push(todo);
+	//const todo = new ToDo("TODO" + listIndex, toDoString, Boolean(x));
+	const todo = {} 
+		todo.name = "TODO" + listIndex;
+		todo.info = toDoString;
+		//todo.checked = false;	
+		//todo.checker = function() { 
+			//if(todo.checker = false) {
+				//console.log("True");
+				//return true;
+		//	}
+			//else {
+				//console.log("False");
+				//return false;
+			//}
+		//}	
+	list.innerHTML += '<input type="checkbox" class=todo-checkbox value="TODO">' + todo.info + '<br>';
+	todoList.push(todo);
+	
 }
-
-
-/*
- * Known bugs:
- * Item count is incremented regardless of whether new item is actually added
- */
